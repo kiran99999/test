dac3094df22324bd93c0072f690f8f069ad7c305
kiran0343 <kiran.kancharla92@gmail.com>
2017-07-01 19:34:41 +0000

    appended html

--- kiran/kiran.py
+++ kiran/kiran.py
@@ -3,7 +3,7 @@ b = 1
 while(a > 0):
     try:
         c = a/b
-        print "c value :",str(c)
+        print "c has value :",str(c)
         break
     except:
         print "a cannot be divided by zero"
